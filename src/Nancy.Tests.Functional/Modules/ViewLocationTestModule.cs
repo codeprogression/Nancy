f8bebe036c3ed06f5a8725439ed7d3d0acf1a344
Richard Cirerol <richard@codeprogression.com>
2012-12-26 09:56:03 -0700

    Adding functional test for modulpath with regex
    Added tests for conventions 0, 2, 4, 6 (all view files located in the views folder).

---
+++ src/Nancy.Tests.Functional/Modules/ViewLocationTestModule.cs
@@ -0,0 +1,11 @@
+﻿namespace Nancy.Tests.Functional.Modules
+{
+    public class ViewLocationTestModule : NancyModule
+    {
+        public ViewLocationTestModule()
+            : base(@"/(?<name>[\w]+)")
+        {
+            Get["/test"] = _ => View["LocationTest"];
+        }
+    }
+}
---
+++ src/Nancy.Tests.Functional/Tests/ViewLocationTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Nancy.Bootstrapper;
+using Nancy.Testing;
+using Nancy.Tests.Functional.Modules;
+using Xunit;
+
+namespace Nancy.Tests.Functional.Tests
+{
+    public class ViewLocationTest
+    {
+        readonly INancyBootstrapper bootstrapper;
+
+        readonly Browser browser;
+
+        public ViewLocationTest()
+        {
+            this.bootstrapper = new ConfigurableBootstrapper(
+                configuration => configuration.Modules(new Type[] {typeof (ViewLocationTestModule)}));
+
+            this.browser = new Browser(bootstrapper);
+        }
+
+
+        [Fact]
+        public void Should_render_content_using_convention_0()
+        {
+            // Given
+            // When
+            var response = browser.Get(
+                @"/viewconvention0/test",
+                with =>
+                {
+                    with.HttpRequest();
+                });
+
+            // Then
+            Assert.True(response.Body.AsString().Contains(@"0 Handles: views / *modulepath* / *modulename* / *viewname*"));
+        }
+
+        [Fact]
+        public void Should_render_content_using_convention_2()
+        {
+            // Given
+            // When
+            var response = browser.Get(
+                @"/viewconvention2/test",
+                with =>
+                {
+                    with.HttpRequest();
+                });
+
+            // Then
+            Assert.True(response.Body.AsString().Contains(@"2 Handles: views / *modulepath* / *viewname*"));
+        }
+
+        [Fact]
+        public void Should_render_content_using_convention_4()
+        {
+            // Given
+            // When
+            var response = browser.Get(
+                @"/viewconvention4/test",
+                with =>
+                {
+                    with.HttpRequest();
+                });
+
+            // Then
+            Assert.True(response.Body.AsString().Contains(@"4 Handles: views / *modulename* / *viewname*"));
+        }
+
+        [Fact]
+        public void Should_render_content_using_convention_6()
+        {
+            // Given
+            // When
+            var response = browser.Get(
+                @"/viewconvention6/test",
+                with =>
+                {
+                    with.HttpRequest();
+                });
+
+            // Then
+            Assert.True(response.Body.AsString().Contains(@"6 Handles: views / *viewname*"));
+        }
+    }
+}
